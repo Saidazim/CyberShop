61001572b6d256e57085b44e4ef971e243edc292
Saidazim <saidazim1416@gmail.com>
2019-01-27 00:44:24 +0500

    chore: import NgCardModule to products module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NbCardModule } from '@nebular/theme';
+
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [ProductListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NbCardModule
   ]
 })
 export class ProductsModule { }
