2e63efe8151d939b091ef8d16c14ea602c7d7da1
Saidazim <saidazim1416@gmail.com>
2019-02-07 04:17:36 +0500

    feat: created reactive form for product dialog

--- src/app/admin/product-edit/product-edit.component.ts
+++ src/app/admin/product-edit/product-edit.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import { UpdateProduct, AddProduct } from 'src/app/products/store/product.actions';
+import { Product } from 'src/app/products/product.model';
+import { AppState } from 'src/app/app.reducers';
+
+export interface DialogData{
+  editMode: boolean,
+  product: Product,
+  index: number
+  }
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +20,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  productForm: FormGroup
+
+  constructor(private store: Store<AppState>,
+    public dialogRef: MatDialogRef<ProductEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm()
+  }
+
+  initForm() {
+    if (this.data.editMode) {
+      this.productForm =this.fb.group({
+        name: [this.data.product.name, Validators.required],
+        price: [this.data.product.price, Validators.required],
+        description: [this.data.product.description, Validators.required],
+      })
+    } else {
+      this.productForm = this.fb.group({
+        name: ['', Validators.required],
+        price: [0, Validators.required],
+        description: ['', Validators.required],
+      })
+    }
+  }
+
+  onSubmit() {
+    const form = this.productForm.value
+    
+    if (this.data.editMode) {
+      this.store.dispatch(new UpdateProduct({
+        product: form,
+        index: this.data.index
+      }))
+    } else {
+      this.store.dispatch(new AddProduct(form))
+    }
+    this.dialogRef.close(form)
+  }
+
+  onCancel() {
+    this.dialogRef.close()
   }
 
 }
