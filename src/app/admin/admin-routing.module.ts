692ed6e10992ef958bd59cd7ef30b96d03fbce51
Saidazim <saidazim1416@gmail.com>
2019-02-08 01:11:06 +0500

    chore: moved layout from product control to admin layout component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { ProductControlComponent } from './product-control/product-control.component';
+import { CategoryControlComponent } from './category-control/category-control.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: ProductControlComponent
-  }
+    path: '', component: AdminLayoutComponent, children: [
+      { path: 'products', component: ProductControlComponent },
+      { path: 'categories', component: CategoryControlComponent },
+  ]}
 ];
 
 @NgModule({
