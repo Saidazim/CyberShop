19c55b3e4c13e9bdcb3d4c3c9e2547ed6fb18ccb
Saidazim <saidazim1416@gmail.com>
2019-02-08 01:34:48 +0500

    feat: added matDialog calling feature

--- src/app/admin/category-control/category-control.component.ts
+++ src/app/admin/category-control/category-control.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 import { Category } from 'src/app/stores/category-store/category.model';
 import { AppState } from 'src/app/stores/app.reducers';
+import { CategoryEditComponent } from '../category-edit/category-edit.component';
 
 @Component({
   selector: 'app-category-control',
@@ -15,11 +17,33 @@ export class CategoryControlComponent implements OnInit {
   categories: Observable<Category[]>
   displayedColumns: string[] = ['name', 'actions'];
   
-  constructor(private store: Store<AppState>, ) {
+  constructor(private store: Store<AppState>, public dialog: MatDialog) {
     this.categories = this.store.select('category')
    }
 
   ngOnInit() {
+  }
+
+  onAdd() {
+    this.openDialog({
+      editMode: false,
+    })
+  }
+
+  onEdit(category: Category, index: number) {
+    this.openDialog({
+      editMode: true,
+      category,
+      index
+    })
+  }
+
+  openDialog(data: object): void {
+    const dialogRef = this.dialog.open(CategoryEditComponent, { data })
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+    });
   }
 
 }
