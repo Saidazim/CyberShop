82f0ac2b93e19622dc35a97164f92246f264f143
Saidazim <saidazim1416@gmail.com>
2019-04-23 07:25:06 +0500

    refactor: replaced , with ; in interfaces

--- src/app/stores/product-store/product.model.ts
+++ src/app/stores/product-store/product.model.ts
@@ -1,13 +1,13 @@
 export interface Product {
-  id: string,
-  name: string,
-  price: number,
-  description: string,
-  photo: string,
-  category: string,
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  photo: string;
+  category: string;
 }
 
 export interface ProductState {
-  productList: Product[],
-  filteredProducts: Product[],
+  productList: Product[];
+  filteredProducts: Product[];
 }
